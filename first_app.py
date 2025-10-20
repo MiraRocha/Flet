b0ba0fabaab0987319ddf00c4e3d7bb6aefb4241
MiraRocha <MiraRocha@users.noreply.github.com>
2025-10-20 22:19:02 +0100

    incio da criação da app

---
+++ first_app.py
@@ -0,0 +1,22 @@
+from flet import *
+
+def main(page: Page):
+    BG = '#041955'
+    FWG = '#97b4ff'
+    FG = '#3450a1'
+    PINK = '#eb06ff'
+    container = Container(
+        width=400,height=850,
+        bgcolor=BG, 
+        border_radius=35,
+        content=Stack(
+            controls=[
+                page_1,
+                page_2
+            ]
+        )
+        )
+    page.add(container)
+
+
+app(target=main)
