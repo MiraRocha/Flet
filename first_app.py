82a9f39f0755ee59cececd7ab8af49d4af84dd05
MiraRocha <166237590+MiraRocha@users.noreply.github.com>
2025-10-21 11:24:05 +0100

    +

--- first_app.py
+++ first_app.py
@@ -1,6 +1,10 @@
 from flet import *
+import flet as ft
+
 
 def main(page: Page):
+    
+    page.add(container)
     BG = '#041955'
     FWG = '#97b4ff'
     FG = '#3450a1'
@@ -9,8 +13,9 @@ def main(page: Page):
         width=400,height=850,
         bgcolor=BG, 
         border_radius=35,
+        
         )
-    page.add(container)
+    
 
 
 app(target=main)
