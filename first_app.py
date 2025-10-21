83a51ebe467006a1afb5a93f568e3abdf4756f9a
MiraRocha <166237590+MiraRocha@users.noreply.github.com>
2025-10-21 12:05:39 +0100

    atualização de criação da pagina 2

--- first_app.py
+++ first_app.py
@@ -1,21 +1,39 @@
 from flet import *
-import flet as ft
-
 
 def main(page: Page):
-    
-    page.add(container)
     BG = '#041955'
-    FWG = '#97b4ff'
+    FMG = '#97b4ff'
     FG = '#3450a1'
     PINK = '#eb06ff'
-    container = Container(
-        width=400,height=850,
-        bgcolor=BG, 
-        border_radius=35,
-        
-        )
     
-
+    page_1 = Container(
+    page_2 = Row(
+        controls=[
+            Container( width = 400,
+                height = 850,
+                bgcolor = FG,
+                border_radius = 35,
+                pdding=padding.only(top=50, left=50, right=20, bottom=5
+                    )
+                )
+            ]
+        )
+    )
+    container = Container(
+        width=400,
+        height=850,
+        bgcolor=BG,
+        border_radius=35
+        content= Stack(
+            controls = [
+                page_1,
+                page_2,
+            ]
+        )
+    )
+        
+    
+    
+    page.add(container)
 
 app(target=main)
