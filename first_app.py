b297f8e13bf0ffc60402669ec7fe80b5101fb1b0
MiraRocha <166237590+MiraRocha@users.noreply.github.com>
2025-10-22 16:56:32 +0100

    atualização da contrução da app

--- first_app.py
+++ first_app.py
@@ -6,18 +6,78 @@ def main(page: Page):
     FG = '#3450a1'
     PINK = '#eb06ff'
     
+    tasks = Column()
+    
+    categories_card = Row(  
+        scroll='auto'
+    )
+    categories = ['Business', 'Family', 'Friends']
+    for index, category in enumerate(categories):
+        categories_card.controls.append(
+            Container(
+                border_radius=20,
+                bgcolor=BG, 
+                height=110, 
+                width=170,
+                padding=15,
+                content=Column(
+                    controls=[
+                        Text('40 Tasks'),
+                        Text(category),
+                        Container(
+                            width=160,
+                            height=5,
+                            bgcolor='white12',
+                            border_radius=20,
+                            padding=padding.only(right=index*30),
+                            content= Container(
+                                bgcolor=PINK,
+                            ),
+                        )
+                    ]
+                )
+
+            )
+        )
+
     first_page_contents =  Container(
         content=Column(
             controls=[
-                Row(
+                Row(alignment='spaceBetween',
                     controls=[
                         Container(
                             content=Icon(
-                                Icons.MENU))
+                                Icons.MENU)),
+                        Row(
+                            controls=[
+                                Icon(Icons.SEARCH),
+                                Icon(Icons.NOTIFICATIONS_OUTLINED)
+
+                                ],
+                            ),
+                    ],
+                ),
+                Container(height=20),
+                Text(
+                    value='What\'s up, Mira!'
+                ),
+                Text(
+                    value='CATEGORIES'
+                ),
+                Container(
+                    padding=padding.only(top=10, bottom=20),
+                    content=categories_card
+                ),
+                Container(height=20),
+                Text("TODAY'S TASKS"),
+                Stack(
+                    controls=[
+                        tasks,
+                        #FloatingActionButton()
                     ]
                 )
-            ]
-        )
+            ],
+        ),
     )
 
     page_1 = Container()
@@ -27,8 +87,7 @@ def main(page: Page):
                 height = 850,
                 bgcolor = FG,
                 border_radius = 35,
-                padding=padding.only(top=50, left=50, right=20, bottom=5
-                ),
+                padding=padding.only(top=50, left=50, right=20, bottom=5),
                 content= Column(
                     controls=[
                         first_page_contents
@@ -47,12 +106,12 @@ def main(page: Page):
             controls = [
                 page_1,
                 page_2,
-            ]
-        )
+            ],
+        ),
     )
         
     
     
     page.add(container)
 
-app(target=main)
+app(target=main,  port=0)
